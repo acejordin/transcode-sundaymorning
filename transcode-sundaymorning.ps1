3bd91a151d6c5a09d7a17f5d4a84ffc3ac1efbf0
Alec Paulson <acejording@gmail.com>
2020-12-01 16:07:31 -0700

    Cleanup, writes to log file, returns an exit code

--- transcode-sundaymorning.ps1
+++ transcode-sundaymorning.ps1
@@ -1,42 +1,54 @@
-Write-Host "Starting transcode-sundaymorning.ps1..."
+Write-Output 'Starting transcode-sundaymorning.ps1...' | Out-File
 $path = 'C:\Users\acejo\Videos\TV\CBS Sunday Morning (1979)'
+#$path = 'C:\Users\acejo\Videos\TV\tasteMAKERS (2018)'
 $cmd = './HandBrakeCLI.exe'
 $destinationFolder = 'T:\tv\CBS Sunday Morning (1979)'
+#$destinationFolder = 'C:\Users\acejo\Videos\TV\tasteMAKERS (2018)\Season 02\Transcoded\'
 $LogFileName = 'transcode-sundaymorning-' + (Get-Date -UFormat "%Y-%m-%d").tostring() + '.log'
-#Write-Host $LogFileName
+#Write-Output $LogFileName
+$PSDefaultParameterValues['Out-File:FilePath'] = $LogFileName
+$PSDefaultParameterValues['Out-File:Append'] = $true
+$exitCode = 0
 
-Get-ChildItem $path -Filter *.ts -Recurse | 
-Foreach-Object {
-
-    Write-Host $_.FullName
-    #Write-Host $_.FullName
-    #Write-Host $_.Name
-    #Write-Host $_.DirectoryName
-    $outputFileName = Join-Path -Path $($_.DirectoryName) -ChildPath ($_.BaseName + '-1.m4v')
-    #Write-Host $outputFileName
-    $destinationFolder = Join-Path -Path $destinationFolder -ChildPath $_.Directory.Name
-    #Write-Host $destinationFolder
+$tsFiles = @(Get-ChildItem $path -Filter *.ts -Recurse)
+Write-Output "Found $($tsFiles.Length) sundaymornings to encode..." | Out-File
+foreach ($tsFile in $tsFiles)
+{
+    Write-Output "Working on $($tsFile.FullName)..." | Out-File
+    $outputFileName = Join-Path -Path $($tsFile.DirectoryName) -ChildPath ($tsFile.BaseName + '-1.m4v')
+    #Write-Output $outputFileName
+    $destinationFolder = Join-Path -Path $destinationFolder -ChildPath $tsFile.Directory.Name
+    #Write-Output $destinationFolder
+    Write-Output 'Beginning HandbrakeCLI encode...' | Out-File
+    & $cmd --preset-import-file preset.json -Z sundaymorning --audio-lang-list "English" --all-audio -i "$($tsFile.FullName)" -o "$outputFileName" 2>> $LogFileName
+    #Write-Output "HandbrakeCLI result: $? $LastExitCode" | Out-File
+    #Write-Output "HandbrakeCLI result: $? $LastExitCode" | Out-File
     
-    & $cmd --preset-import-file preset.json -Z sundaymorning --audio-lang-list "English" --all-audio -i "$($_.FullName)" -o "$outputFileName" 2>> $LogFileName
-    Write-Host $?
-    if(-Not $?)
+    if($LastExitCode -ne 0)
     {
+        Write-Output 'Failed encode; continuing to next' | Out-File
+        $exitCode = 1
         continue #if handbrakecli isn't successful don't copy or delete anything, move on to next file
     }
 
-    #Copy-Item -Path 'C:\Users\acejo\repos\transcode-sundaymorning\help.txt' -Destination 'C:\Users\acejo\repos\transcode-sundaymorning\help2.txt' #Copy-Item -Path $outputFileName -Destination $destinationFolder
+    Write-Host 'Finished encode, copying to tower...' | Out-File
+    New-Item -Force $destinationFolder -ItemType "directory" #this will create the folder if it doesn't exist
+    
     Copy-Item -Path $outputFileName -Destination $destinationFolder
-    Write-Host $?
-    if (-Not $?) #Copy-Item was false
+    Write-Host $? | Out-File
+    if ($? -eq $false) #Copy-Item was false
     {
+        Write-Host 'Copy to tower was unsuccessful; continuing to next' | Out-File
+        $exitCode = 1
         continue #If copy was unsuccessful then don't delete, move on to next file
     }
 
+    Write-Host 'Renaming .ts and local .m4v' | Out-File
     #uncomment these out when sure copy/script is good
-    #Remove-Item $_.FullName #delete the .ts file
+    #Remove-Item $tsFile.FullName #delete the .ts file
     #Remove-Item $outputFileName #delete the local .m4v file
     #rename them until we start to delete them
-    Rename-Item $_.FullName ($_.FullName + '.old')
+    Rename-Item $tsFile.FullName ($tsFile.FullName + '.old')
     Rename-Item $outputFileName ($outputFileName + '.old')
 
     break #just do one file for now
@@ -46,4 +58,9 @@ Foreach-Object {
     #validate handbrakecli was successful?
     #copy-item the file to tower, validate success
     #remove-item the .ts file
+
 }
+
+Write-Output 'transcode-sundaymorning.ps1 finished' | Out-File
+
+exit $exitCode
