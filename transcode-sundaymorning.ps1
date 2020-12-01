859b36cbc49269499299002186d3daa289af446c
Alec Paulson <acejording@gmail.com>
2020-11-30 21:28:00 -0700

    almost there

--- transcode-sundaymorning.ps1
+++ transcode-sundaymorning.ps1
@@ -1,4 +1,4 @@
-Write-Host "Staring transcode-sundaymorning.ps1..."
+Write-Host "Starting transcode-sundaymorning.ps1..."
 $path = 'C:\Users\acejo\Videos\TV\CBS Sunday Morning (1979)'
 $cmd = './HandBrakeCLI.exe'
 $destinationFolder = 'T:\tv\CBS Sunday Morning (1979)'
@@ -8,24 +8,33 @@ $LogFileName = 'transcode-sundaymorning-' + (Get-Date -UFormat "%Y-%m-%d").tostr
 Get-ChildItem $path -Filter *.ts -Recurse | 
 Foreach-Object {
 
+    Write-Host $_.FullName
     #Write-Host $_.FullName
     #Write-Host $_.Name
     #Write-Host $_.DirectoryName
-    $outputFileName = $($_.DirectoryName) + '\' + $($_.BaseName) + '-1.m4v'
-    Write-Host $_.Directory.Name
+    $outputFileName = Join-Path -Path $($_.DirectoryName) -ChildPath ($_.BaseName + '-1.m4v')
+    #Write-Host $outputFileName
     $destinationFolder = Join-Path -Path $destinationFolder -ChildPath $_.Directory.Name
-    Write-Host $destinationFolder
-    #--% is muy importante, see README.md
-    #Write-Host $cmd '--%' '--preset-import-file preset.json' '-v' '-Z "sundaymorning"' '-a 2' '-i' "`"$($_.FullName)`"" '-o' "`"$outputFileName`""
-    & $cmd --preset-import-file preset.json -Z sundaymorning -a 2 -i "$($_.FullName)" -o "$outputFileName" >> $LogFileName
-    Write-Host $?
-    Copy-Item -Path 'C:\Users\acejo\repos\transcode-sundaymorning\help.txt' -Destination 'C:\Users\acejo\repos\transcode-sundaymorning\help2.txt' #Copy-Item -Path $outputFileName -Destination $destinationFolder
-    if ($?) #Copy-Item was true
-    {
-
-    }
+    #Write-Host $destinationFolder
     
+    & $cmd --preset-import-file preset.json -Z sundaymorning --audio-lang-list "English" --all-audio -i "$($_.FullName)" -o "$outputFileName" 2>> $LogFileName
     Write-Host $?
+    if(-Not $?)
+    {
+        continue #if handbrakecli isn't successful don't copy or delete anything, move on to next file
+    }
+
+    #Copy-Item -Path 'C:\Users\acejo\repos\transcode-sundaymorning\help.txt' -Destination 'C:\Users\acejo\repos\transcode-sundaymorning\help2.txt' #Copy-Item -Path $outputFileName -Destination $destinationFolder
+    Copy-Item -Path $outputFileName -Destination $destinationFolder
+    Write-Host $?
+    if (-Not $?) #Copy-Item was false
+    {
+        continue #If copy was unsuccessful then don't delete, move on to next file
+    }
+
+    #uncomment these out when sure copy/script is good
+    #Remove-Item $_.FullName #delete the .ts file
+    #Remove-Item $outputFileName #delete the local .m4v file
 
     break
 
@@ -33,5 +42,5 @@ Foreach-Object {
     #better comment script
     #validate handbrakecli was successful?
     #copy-item the file to tower, validate success
-    #delete .ts file
+    #remove-item the .ts file
 }
